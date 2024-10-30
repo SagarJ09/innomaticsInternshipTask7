d550b2f8be13c0a3847428bd0e54308e61de6a43
SagarS360 <sagarpeaceful@gmail.com>
2024-10-30 20:43:19 +0530

    UpDated

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-do
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
+import ServiceDetails from './components/ServicesDetails';
 import Contact from './components/Contact';
 
 const App = () => {
   return (
     <Router>
       <div>
-        {/* Navigation Bar */}
+        
         <nav>
           <NavLink to="/" end activeClassName="active">Home</NavLink>
           <NavLink to="/about" activeClassName="active">About</NavLink>
-          <NavLink to="/services" activeClassName="active">Services</NavLink>
+          <NavLink to="/services" activeClassName="active">Service</NavLink>
           <NavLink to="/contact" activeClassName="active">Contact</NavLink>
         </nav>
 
